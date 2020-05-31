1126c1ad78c35432809c7504c7a24beedf933275
oleh <grolan12345@gmail.com>
2020-05-31 21:39:14 +0300

    return, шаблонная строка, проверка масива

--- hw-02/js/task-6.js
+++ hw-02/js/task-6.js
@@ -4,16 +4,19 @@ let total = 0;
 
 do {
     input = prompt('Введите число');
-    const isNumber = Number.isNaN(+input);
-    if (isNumber === true) {
+    if (isNaN(input)) {
         alert('Было введено не число, попробуйте еще раз');
         continue;
+    } if (input !== null) {
+        numbers.push(+input);
     }
-    numbers.push(+input);
 } while (input !== null)
 
-for (let number of numbers) {
-    total += number;
+if (numbers.length !== 0) {
+    for (let number of numbers) {
+        total += number;
+    }
+    console.log(`Общая сумма чисел равна ${total}`);
+} else if (numbers.length === 0) {
+    console.log('Вы не ввели число!');
 }
-
-console.log(`Общая сумма чисел равна ${total}`);
